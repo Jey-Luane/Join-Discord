539f09eab052bc56937b01f40b475f2ce6ca1bcd
JeyKrudener <50692730+JeyKrudener@users.noreply.github.com>
2020-09-29 17:22:08 -0300

    Create entrace.py
    Self for Discord app.

---
+++ entrace.py
@@ -0,0 +1,24 @@
+# <---------> - = - = - <--------->
+import discord
+import asyncio
+# <---------> - = - = - <--------->
+client = discord.Client()
+# <---------> - = - = - <--------->
+email = "EMAIL"
+pw = "PW"
+
+# <---------> - = - = - <--------->
+@client.event
+async def on_ready():
+    print("Self Entrace ON")
+    print("Servers: {}".format(str(len(client.servers))))
+    print("OK!")
+
+
+@client.event
+async def on_member_join(member):
+  msg = "Message {} Message \n Message**".format(member.mention)
+  await client.send_message(member, msg) 
+
+
+client.run(email, senha)
